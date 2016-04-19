5b27ba5fd31ca1c43c5768570c3ce121badbc290
Killian <ksc37@pitt.edu>
2016-04-19 15:06:53 -0400

    Created Base

--- index.php
+++ index.php
@@ -1,39 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<?php
+require_once "functions.php";
+$action = (isset($_GET['action'])!=null?$_GET['action']:"home");
 
-    <title>East2.0</title>
+session_start();
+// Just for testing
+$_SESSION['userID'] = 1;
+$_SESSION['userType'] = 0; // = 0 Student, = 1 Staff
 
-    <!-- Bootstrap core CSS -->
-    <link href="support/dist/css/bootstrap.min.css" rel="stylesheet">
+// session_unset(); // Uncomment to view login page
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="support/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+if(!isLoggedIn()){
+			// Display Login Form
+			$page['title'] = "Login";
+			require_once 'header.php';
+			require_once 'login.php';
+			require_once 'footer.php';
+} else {
+	switch ($action) {
+		case "home":	
+			if(isStudent()){
+				$page['title'] = "Student - Class List";
+				$page['studentCourseList'] = getStudentCourses();
+				
+				require_once 'header.php';				
+				require_once 'student/classlist.php';
+			} else {
+				$page['title'] = "Facalty - Class List";
+				require_once 'header.php';
+				echo "Facalty Page";
+			}	
+			
+			require_once 'footer.php';
+			break;  
+		
+		case "course":
+			if(isStudent()){
+				$page['title'] = "Student - Assignment List";	
+				$courseID = $_GET['courseID'];
+				$page['studentCourseAssignments'] = getStudentCourseAssignments($courseID);
+				
+				require_once 'header.php';				
+				require_once 'student/assignlist.php';
+			} else {
+				$page['title'] = "Facalty - Assignment List";
+				require_once 'header.php';
+				echo "Facalty Page";
+			}			
+			
+		break;
+		
+		case "assignment":	
+				if(isStudent()){
+					$page['title'] = "Student - Assignment";
+					$assID = $_GET['assignmentID'];
+					$page['studentAssSubmissions'] = getStudentAssignmentSubmissions($assID);
+					require_once 'header.php';
+					require_once 'student/assignment.php';
+				} else {
+					$page['title'] = "Facalty - Assignment";
+					require_once 'header.php';
+					echo "Facalty Page";
+				}			
+			
+		break;
 
-    <!-- Custom styles for this template -->
-    <link href="styles/signin.css" rel="stylesheet">
 
-  </head>
+		default:
+			echo "You Must Be Lost!";
+	}
+}
 
-  <body>
-
-    <div class="container">
-      <form class="form-signin">
-
-        <h3><p><a href="login.php">Login</a></p></h3>
-        <h3><p><a href="signup.php">Sign up for new account</a></p></h3>
-        <label>Forgot Password: Enter complete pitt e-mail address: </label>
-        <input type="email" id="forgotPasswordEmail" placeholder="Email address">
-
-        <button class="btn btn-primary" type="submit">Send Password</button>
-    </div> <!-- /container -->
-      </form>
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="support/assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
