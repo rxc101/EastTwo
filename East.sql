7e82a718394a845554cc0ee897fefc86a336e192
Killian <ksc37@pitt.edu>
2016-04-27 16:17:44 -0400

    Created DB and Backend Get Querries For Staff

--- East.sql
+++ East.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.12
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Apr 20, 2016 at 03:17 PM
--- Server version: 5.5.46
--- PHP Version: 5.6.14
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2016 at 10:15 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,34 +14,304 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `East`
+-- Database: `east`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Users`
+-- Table structure for table `assignments`
 --
 
-CREATE TABLE IF NOT EXISTS `Users` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `assignments` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `MaxPoints` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `assignments`
+--
+
+INSERT INTO `assignments` (`ID`, `Name`, `MaxPoints`) VALUES
+(1, 'First Assignment ', 23);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignmentsubmissions`
+--
+
+CREATE TABLE `assignmentsubmissions` (
+  `id` int(11) NOT NULL,
+  `AssID` int(11) NOT NULL,
+  `SubID` int(11) NOT NULL,
+  `StudentID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `assignmentsubmissions`
+--
+
+INSERT INTO `assignmentsubmissions` (`id`, `AssID`, `SubID`, `StudentID`) VALUES
+(1, 1, 1, 2),
+(2, 1, 2, 3),
+(3, 1, 3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courseassignments`
+--
+
+CREATE TABLE `courseassignments` (
+  `ID` int(11) NOT NULL,
+  `CourseID` int(11) NOT NULL,
+  `AssignmentID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `courseassignments`
+--
+
+INSERT INTO `courseassignments` (`ID`, `CourseID`, `AssignmentID`) VALUES
+(1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`ID`, `Name`) VALUES
+(1, 'Volley Ball Course'),
+(3, 'Another Class');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `semesters`
+--
+
+CREATE TABLE `semesters` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `semesters`
+--
+
+INSERT INTO `semesters` (`ID`, `Name`) VALUES
+(1, 'Spring 2016'),
+(2, '2015 - Fall'),
+(3, 'Summer -2015');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staffcourses`
+--
+
+CREATE TABLE `staffcourses` (
+  `ID` int(11) NOT NULL,
+  `CourseID` int(11) NOT NULL,
+  `StaffID` int(11) NOT NULL,
+  `SemesterID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staffcourses`
+--
+
+INSERT INTO `staffcourses` (`ID`, `CourseID`, `StaffID`, `SemesterID`) VALUES
+(1, 1, 1, 1),
+(3, 3, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentcourses`
+--
+
+CREATE TABLE `studentcourses` (
+  `ID` int(11) NOT NULL,
+  `StudentID` int(11) NOT NULL,
+  `CourseID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `studentcourses`
+--
+
+INSERT INTO `studentcourses` (`ID`, `StudentID`, `CourseID`) VALUES
+(1, 2, 1),
+(3, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submissions`
+--
+
+CREATE TABLE `submissions` (
+  `ID` int(11) NOT NULL,
+  `comments` varchar(250) NOT NULL,
+  `feedback` varchar(250) NOT NULL,
+  `graded` int(11) DEFAULT NULL,
+  `fileloc` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `submissions`
+--
+
+INSERT INTO `submissions` (`ID`, `comments`, `feedback`, `graded`, `fileloc`) VALUES
+(1, 'my comment', 'feed back ', 25, 'submissions/1/file.png'),
+(2, 'asdf', 'adsf', 2, 'here'),
+(3, 'adsf', 'asdf', NULL, '2341');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
   `Email` varchar(30) NOT NULL,
   `AccountType` int(1) NOT NULL,
-  `Password` varchar(20) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `Password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `Users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `Users` (`Id`, `Email`, `AccountType`, `Password`) VALUES
-(1, 'bob@bob.com', 0, 'password'),
-(2, 'jim@jim.com', 0, 'password');
+INSERT INTO `users` (`Id`, `Email`, `AccountType`, `Password`) VALUES
+(1, 'bob@bob.com', 1, 'password'),
+(2, 'jim@jim.com', 0, 'password'),
+(3, 'kill@kill.com', 0, 'pass');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `assignmentsubmissions`
+--
+ALTER TABLE `assignmentsubmissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courseassignments`
+--
+ALTER TABLE `courseassignments`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `semesters`
+--
+ALTER TABLE `semesters`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `staffcourses`
+--
+ALTER TABLE `staffcourses`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `studentcourses`
+--
+ALTER TABLE `studentcourses`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `submissions`
+--
+ALTER TABLE `submissions`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assignments`
+--
+ALTER TABLE `assignments`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `assignmentsubmissions`
+--
+ALTER TABLE `assignmentsubmissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `courseassignments`
+--
+ALTER TABLE `courseassignments`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `semesters`
+--
+ALTER TABLE `semesters`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `staffcourses`
+--
+ALTER TABLE `staffcourses`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `studentcourses`
+--
+ALTER TABLE `studentcourses`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `submissions`
+--
+ALTER TABLE `submissions`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
