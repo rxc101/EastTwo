4d9801c4720c8a026868300203ef1b0a7a7e11b7
Killian <ksc37@pitt.edu>
2016-04-28 12:57:38 -0400

    Made image responsive

--- new404.php
+++ new404.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="well">
         <h2 class="form-signin-heading">We're sorry but this page does not exist</h2>
-        <img src="giphyCat.gif" /><br /><br />
+        <img class="img-responsive"  src="giphyCat.gif"  /><br /><br />
         <a href="index.php?action=home" ><button class="btn btn-lg btn-primary">Go to East</button></a>
     </div>
 </div> <!-- /container -->
